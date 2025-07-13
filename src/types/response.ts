32bfeb56a18acc1e9eed502087fe27f63e6f19dd
Hamza KAYA <hamzakaya@MacBookPro.home>
2025-07-13 19:45:48 +0300

    response data type

---
+++ src/types/response.ts
@@ -0,0 +1,18 @@
+export type Filter<T> = Partial<T> & {
+  _page?: number;
+  _limit?: number;
+  _search?: string;
+  _sort?: string;
+  _order?: "asc" | "desc";
+  _all?: boolean;
+};
+
+export type Response<T> = {
+  data: T[];
+  meta: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+  };
+};
